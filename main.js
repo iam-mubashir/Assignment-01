4cf9623987f63043cc769c0baf21fa6db246555c
unknown <ahmadmobin07@gmail.com>
2024-03-28 02:35:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// 01 Assignment
+// storing a person name in variable
+let PersonName = "Eric";
+// printing the message to person.
+console.log(`Hello ${PersonName}, would you like to learn some Python today?”`);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Typescript  Assignment 01
+
+
+// storing a person name in variable
+let PersonName: string = "Eric"
+
+// printing the message to person.
+console.log(`Hello ${PersonName}, would you like to learn some Python today?”`)
